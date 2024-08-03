416ea630219a5734ffae369865c4e6bfb8bef554
ms-sophia <jardelezasophia@gmail.com>
2024-08-03 12:26:20 +0800

    may mistake pala yung control-button pero working na yung start and pause/resume timer

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,48 @@
+// let minutes = 60;
+let timer;
+let isPaused = false;
+let minutes = 60;
+function startTimer()
+{
+    timer = setInterval(updateTimer, 1000);
+}
+
+function updateTimer() {
+    var timerElement = document.getElementById('timer');
+    timerElement.textContent = minutes;
+    if (minutes === 0)
+    {
+        clearInterval(timer);
+        alert('Time is up! Take a break');
+        timerElement.textContent = '60';
+    }
+    else if(!isPaused) {
+        minutes--;
+    }
+}
+
+
+function togglePauseResume()
+{
+    var pauseResumeBtn = document.querySelector('#pauseResumeBtn');
+    isPaused = !isPaused;
+    if (isPaused)
+    {
+        clearInterval(timer);
+        pauseResumeBtn.textContent = 'Resume'; 
+    }
+    else
+    {
+        startTimer();
+        pauseResumeBtn.textContent = 'Pause';
+    }
+}
+
+// startTimer();
+// ------------THIS IS FOR CLOCK---------------------//
 
 // Showing the current time on the page
-var showCurrentTime = function ()
+function showCurrentTime()
 {
     // displaying the string on the webpage
     var clock = document.getElementById('clock');
@@ -11,11 +53,16 @@ var showCurrentTime = function ()
     var mins = currentTime.getMinutes();
     var secs = currentTime.getSeconds();
     var meridian = "AM";
-
-    var clockTime = hrs + ':' + mins + ':' + secs + ':' + " " + meridian + '!';
+    
+    if (hrs >= 12)
+    {
+        meridian = "PM";
+    }
+    var clockTime = hrs + ':' + mins + ':' + secs + " " + meridian + '!';
 
     clock.innerText = clockTime
+    
+    // Updates the seconds
+    setInterval(showCurrentTime,1000)
 }
-
-showCurrentTime();
-
+// showCurrentTime();
