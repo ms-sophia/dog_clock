0b41646e7cea2dffc679e5eaaa7ee288eeef5bbd
ms-sophia <jardelezasophia@gmail.com>
2024-08-06 12:46:31 +0800

    changes made

--- scripts/script.js
+++ scripts/script.js
@@ -91,17 +91,22 @@ function gifChanger(minutes)
     else if (minutes < 15)
     {
         image = image = "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExZml1dnp6Mm5ldzFxcXUyaDZhbXZ4b2EycWI5Nm96d2FjZTVsaTNraCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Pv3GlvjHv1T1u/giphy.gif"
-        messageText =  "Work betch"
+        messageText =  "Werk Mode"
     }
     else if (minutes < 30)
     {
         image = "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExbW51ODNkdHFnOXlyc2k3NmUydGNxOTA4ajFuNnM4NmxjaDk4MGliMCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/bswGDO7Rh1ONO/giphy.gif"
         messageText = "Break time!!!!!"
     }
-    else
+    else if (minutes < 60)
     {
         image = "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExbWY1czFoMnl0N2xjNHZobHlxMm02amVnZ3R1eTlweXMxOHlrajI3ciZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/XdPCms1PVHESm8XR9c/giphy.gif"
         messageText = "Wurk Mode"
+    }
+    else
+    {
+        image = "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExOHJybzJqdzY1dG82YjJ1NHV4bWsxdWphem5tdmoybDJ6Z2Nhamt0cCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/gKHGnB1ml0moQdjhEJ/giphy.gif"
+        messageText = "No Mode"
     }
 
 
